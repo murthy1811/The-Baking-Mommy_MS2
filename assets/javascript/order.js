a355e0a973d676007d7f09d67a455690013f8a1c
Murthy Valavala <murthy.mailme@gmail.com>
2021-05-24 19:33:59 +0000

    maps API added in order.js

---
+++ assets/javascript/order.js
@@ -0,0 +1,33 @@
+             function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 13,
+                center: {
+                    lat: 57.7089, 
+                    lng: 11.9746
+                }
+            });
+            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+            var locations = [{
+                lat: 57.7067117, 
+                lng: 11.9669017
+            }, {
+                lat: 57.7208338,
+                lng: 11.9514833
+            }, {
+                lat: 57.7109309,
+                lng: 11.9456557
+            }];
+
+            var markers = locations.map(function (location, i) {
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.length]
+                });
+            });
+
+            var markerCluster = new MarkerClusterer(map, markers, {
+                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+            });
+        }
+   
