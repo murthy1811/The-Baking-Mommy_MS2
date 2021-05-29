0e69698c827d12ed26ec43d728036cf4023b589b
Murthy Valavala <murthy.mailme@gmail.com>
2021-05-29 17:42:59 +0000

    js added for the subscribe button

---
+++ assets/javascript/prices.js
@@ -0,0 +1,35 @@
+$('#subscribe').prop('disabled', true);
+
+let toValidate = jQuery('#mailbox'),
+    valid = false;
+toValidate.change(function () {
+    if (jQuery(this).val().length > 0) {
+        jQuery(this).data('valid', true);
+    } else {
+        jQuery(this).data('valid', false);
+    }
+    toValidate.each(function () {
+        if (jQuery(this).data('valid') == true) {
+            valid = true;
+        } else {
+            valid = false;
+        }
+    });
+    if (valid === true) {
+        jQuery("#subscribe").prop('disabled', false);
+    } else {
+        jQuery("#subscribe").prop('disabled', true);
+    }
+    
+});
+
+
+
+    document.getElementById("subscribe").addEventListener("click", sweetalert);
+    function sweetalert(){
+
+    Swal.fire("Thank you for the subscription", "Happy Eating!");
+
+    document.getElementById("subscribe").reset();
+    return false;
+}
