6cd44d64c566176f2da319dfe8245f70b1a00506
Murthy Valavala <murthy.mailme@gmail.com>
2021-05-30 19:55:09 +0000

    media query added for feedback

--- assets/javascript/feedback.js
+++ assets/javascript/feedback.js
@@ -1,6 +1,34 @@
 
 // JS for star ratings ( code credit to : https://www.markuptag.com/feedback-form-with-star-rating-html/)
 
+
+$('.submit-rating').prop('disabled', true);
+
+$("input[type=radio]").one("click", function() { $(".submit-rating").prop("disabled", false); });
+
+let toValidate = jQuery('#choice'),
+    valid = false;
+toValidate.change(function () {
+    if (jQuery(this).val().length > 0) {
+        jQuery(this).data('valid', true);
+    } else {
+        jQuery(this).data('valid', false);
+    }
+    toValidate.each(function () {
+        if (jQuery(this).data('valid') == true) {
+            valid = true;
+        } else {
+            valid = false;
+        }
+    });
+    if (valid === true) {
+        jQuery(".submit-rating").prop('disabled', false);
+    } else {
+        jQuery(".submit-rating").prop('disabled', true);
+    }
+    
+});
+
 const btn = document.querySelector(".submit-rating");
 const thanksmsg = document.querySelector(".thanks-msg");
 const starRating = document.querySelector(".star-input");
