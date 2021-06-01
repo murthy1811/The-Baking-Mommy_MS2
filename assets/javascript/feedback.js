e0ae6eac94b764d8737ff8da16df6c03346aa072
Murthy Valavala <murthy.mailme@gmail.com>
2021-06-01 18:54:26 +0000

    emailjs succeeded for feedback form

--- assets/javascript/feedback.js
+++ assets/javascript/feedback.js
@@ -1,24 +1,26 @@
- 
-window.onload = function() {
+ window.onload = function() {
     document.getElementById('feedbackform').addEventListener('submit', function(event) {
         event.preventDefault();
         Swal.fire("Thank you for your feedback!");
         document.getElementById("feedbackform").reset();
         // send email here
 
-        emailjs.send("service_xkqr1dk","template_8p5dh3t", {
+            });
+}
+
+function sendMail(contactForm){
+                emailjs.send("service_xkqr1dk","template_8p5dh3t", {
             "caketype": cakeChoice,
-            "cakeflavour":event.choice.value,
+            "cakeflavour": contactForm.choice.value,
             "cakerating": cakeRating,
             "recommendation": cakeRecommend,
-            "tellus": event.comments.value,
+            "tellus": contactForm.comments.value,
             "starrating": overallRating
             
         });
+    }
 
 
-    });
-}
 
 
 
